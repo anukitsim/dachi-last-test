257ccf4436d1da68dba73b18b315f6deebbbd886
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-10-26 16:57:27 +0400

    added psr page, test for deploy

--- app/.OBJ/page.jsx
+++ app/.OBJ/page.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Image from "next/image";
 
 const Obj = () => {
   return (
-    <div className="flex  flex-col items-center transform translate-x-0.5 translate-y-36 mt-1">Obj</div>
+    <div className="md:grid md:grid-cols-12 md:grid-rows-2 md:transform md:translate-x-0.5 md:translate-y-36  mt-1 sm:flex sm:flex-col sm:m-5">
+      <Image
+        src="/images/obj1.png"
+        alt="invasive-1"
+        width={500}
+        height={500}
+        sizes="(min-width: 1740px) 41.67vw, (min-width: 700px) calc(51.67vw - 172px), 55.53vw"
+        className="md:w-full sm:w-full h-auto z-0 md:col-start-1 md:row-start-1 md:col-span-6"
+      />
+      <Image
+        src="/images/obj2.png"
+        alt="invasive-1"
+        width={500}
+        height={500}
+        sizes="(min-width: 1740px) 41.67vw, (min-width: 700px) calc(51.67vw - 172px), 55.53vw"
+        className="md:w-full sm:w-full h-auto z-0 md:col-start-6 md:row-start-1  md:col-span-6"
+      />
+       <Image
+        src='/images/Text.png'
+        alt="invasive-1"
+        width={500}
+        height={500}
+        sizes="(min-width: 1740px) 41.67vw, (min-width: 700px) calc(51.67vw - 172px), 55.53vw"
+        className="md:w-full sm:w-full md:col-start-5 md:row-start-1 md:col-span-3 mt-10 z-50"
+      />
+      
+      
+    </div>
   )
 }
 
-export default Obj
+export default Obj;
--- app/PSR/page.jsx
+++ app/PSR/page.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+
+"use client"
+
+import { useEffect } from "react";
+
 
 function Psr() {
+  useEffect(() => {
+    const audio = new Audio('/audio/psr.mp3');
+    audio.loop = true; 
+    audio.play();
+
+    return () => {
+      audio.pause();
+      audio.src = '';
+    };
+  }, []);
+
   return (
-    <div className="flex  flex-col items-center transform translate-x-0.5 translate-y-36 mt-1">Psr</div>
+    <>
+    <div className="absolute top-0 left-0 z-0 w-screen h-full">
+      <div className="z-10 bg-cover md:mt-[-40%] bg-no-repeat">
+      <video className="w-full h-full object-cover" autoPlay muted playsInline loop>
+        <source src="/video/gif.mp4" type="video/mp4" />
+      </video>
+      </div>
+    </div>
+  </>
   )
 }
 
